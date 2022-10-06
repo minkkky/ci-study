f7bdf890a96cc4236b94586e89e5ea0d41645a98
m_ink <mp98dl4@gmail.com>
2022-10-06 22:17:41 +0900

    ENH test code add

---
+++ sunset_ninja/tests/test_view.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+
+
+class TestView(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        import django
+        django.setup()
+
+    def test_add_view(self) -> None:
+        result = self.client.get("/api/add", {"a":1, "b":3})
+        self.assertEqual(result.status_code, 200)
+        self.assertEqual(result.json(), {"result":4})
