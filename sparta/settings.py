996a86284e30a62e3cc48678ccf77d7356e1f3e5
m_ink <mp98dl4@gmail.com>
2022-10-19 01:41:23 +0900

    edit settings.py

--- sparta/settings.py
+++ sparta/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import os
 from pathlib import Path
 from typing import List
 
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ""
+SECRET_KEY = "django-insecure-m(i0ecf5qx5lpjlp!ng9#b^se%z@j!zgfi2tceo^=ly1c#j*q8"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -83,6 +84,7 @@ DATABASES = {
         "USER": "user",
         "PASSWORD": "password",
         "HOST": "localhost",
+        
         "PORT": "",
     }
 }
